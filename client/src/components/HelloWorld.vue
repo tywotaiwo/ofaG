a6bcbfd49b3bb29073dd4f96ac2bcb5f36c453f0
tywotaiwo <32190805+tywotaiwo@users.noreply.github.com>
2018-08-16 16:08:56 +0300

    first commit, login and registration with mysql backend completed

---
+++ client/src/components/HelloWorld.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="hello">
+    <h2>{{ msg1 }}</h2>
+    <h2>{{ msg2 }}</h2>
+    <h2>{{ msg3 }}</h2>
+    <h2><a href="#">Register</a></h2>
+
+    <h2><a href="#">Login</a></h2>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'index',
+  data () {
+    return {
+      msg1: 'Shop for someone',
+      msg2: 'or',
+      msg3: 'Find a shopper from anywhere in the world'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+a {
+  color: #42b983;
+  text-decoration: none;
+}
+</style>
---
+++ client/src/components/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="hello">
+    <h3>{{ msg1 }}</h3>
+    <h3>{{ msg2 }}</h3>
+    <h3>{{ msg3 }}</h3>
+    <h2><a href="#">Register</a></h2>
+
+    <h2><a href="#">Login</a></h2>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'index',
+  data () {
+    return {
+      msg1: 'Shop for someone',
+      msg2: 'or',
+      msg3: 'Find a shopper from anywhere in the world'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h3 {
+  font-weight: normal;
+}
+a {
+  color: #b9393f;
+  text-decoration: none;
+}
+</style>
